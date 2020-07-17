2d33f2250922cf4ad73cbe06fed06a70076620a2
Kimberly Lord <kalord93@gmail.com>
2020-07-17 14:30:17 -0600

    Updates

---
+++ precache-manifest.23ebbd701c279842c7047df88a21e23d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40593724c5d210e16664e8f612ee82af",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "fee7e3126c30fe26e2f7",
+    "url": "/project-4-assessment/static/css/main.77f64eaf.chunk.css"
+  },
+  {
+    "revision": "4cead443ffe1bb4d6058",
+    "url": "/project-4-assessment/static/js/2.a97551f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.a97551f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fee7e3126c30fe26e2f7",
+    "url": "/project-4-assessment/static/js/main.ec8324c8.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  }
+]);
